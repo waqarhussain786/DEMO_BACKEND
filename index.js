df8d7204c42c37d12218abee85996dfeb056aa63
waqar hussain ujjan <wh751212@gmail.com>
2026-03-16 15:07:28 +0500

    a

---
+++ index.js
@@ -0,0 +1,32 @@
+// 1. Import the express library
+const express = require('express');
+
+// 2. Initialize the express application
+const app = express();
+
+// 3. Define the Port number
+const PORT = 3000;
+
+app.use(express.json());
+
+// 4. Create a basic "Route" (The home page)
+app.get('/', (req, res) => {
+    res.send('Server is running successfully!');
+});
+
+// This route receives data
+app.post('/api/user', (req, res) => {
+    const userData = req.body; // This is the data sent from the frontend
+    console.log("Data received:", userData);
+
+    // Sending a response back to the frontend
+    res.json({
+        message: "User data received successfully!",
+        receivedData: userData
+    });
+});
+
+// 5. Start the server and listen for requests
+app.listen(PORT, () => {
+    console.log(`🚀 Server is live at http://localhost:${PORT}`);
+});
